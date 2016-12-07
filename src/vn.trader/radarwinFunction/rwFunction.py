598c97c4234a6ba65d2ab8bbddfd590b3e220247
vnpyRadarwin <owen.pan@radarwin.com>
2016-12-07 15:03:30 +0800

    注释修改

--- src/vn.trader/radarwinFunction/rwFunction.py
+++ src/vn.trader/radarwinFunction/rwFunction.py
@@ -11,15 +11,18 @@
 # positionDict:当前账户信息
 # pos:买/卖量
 # price：买/卖价格
+#返回值：False-》不可交易，True-》可交易
 def getPosition(params, positionDict, pos, price):
     if params == 'buy':
-        posData = positionDict['cny']
-        if posData.position < pos * price:
-            return False
+        if 'cny' in positionDict:
+            posData = positionDict['cny']
+            if posData.position < pos * price:
+                return False
     elif params == 'sell':
-        posData = positionDict['btc']
-        if posData.position < pos:
-            return False
+        if 'btc' in positionDict:
+            posData = positionDict['btc']
+            if posData.position < pos:
+                return False
     return True
 
 
